0cdc3ae306acd28636209121dcb944055edd3f02
Mishel Baretto <mishel.baretto@axionet.in>
2023-03-15 20:06:13 +0530

    Angular15 with >net6 WEB API

---
+++ FullStack API/FullStack API/FullStack API/Data/FullStackDBContext.cs
@@ -0,0 +1,13 @@
+﻿using FullStack_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack_API.Data
+{
+    public class FullStackDBContext : DbContext
+    {
+        public FullStackDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
